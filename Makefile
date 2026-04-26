64a5fbd985c21834fafb6f2b1c1921bdb0117f78
mattman4 <91604143+mattman4@users.noreply.github.com>
2026-04-26 10:08:41 +0100

    Initial file reading. Switch to CMake.

--- Makefile
+++
@@ -1,7 +0,0 @@
-CXX = g++
-SRC = $(wildcard src/*.cpp)
-OUT = trynd
-INCLUDES = -Iinclude
-
-build:
-	$(CXX) $(INCLUDES) $(SRC) -o $(OUT)
--- src/Trynd.cpp
+++ src/Trynd.cpp
@@ -1,6 +1,41 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+void runFile(const std::string&);
 
 int main(int argc, char* argv[]) {
-    printf("Hello, World!\n");
-    return 0;
+
+    if(argc > 2) {
+        std::cout << "Usage: trynd [<file>]" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if(argc == 2) {
+        runFile(argv[1]);
+    } else {
+        // Run prompt
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void runFile(const std::string& path) {
+    std::ifstream file(path, std::ios::binary);
+
+    if(!file) {
+        std::cout << "Error: file '" << path << "' not found" << std::endl; 
+        std::exit(EXIT_FAILURE);
+    }
+
+    // Find out size
+    file.seekg(0, std::ios::end);
+    const std::streampos size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> chars(size);
+    file.read(chars.data(), size);
+
+    for(auto c : chars) std::cout << c;
 }
